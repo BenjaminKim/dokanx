3a97b883b7ddf8040417330db6a6c6f11289acae
Benjamin <jehokim@live.com>
2015-02-22 02:07:40 +0900

    researching sshfs.

---
+++ Sysinternals.Debug/ProcessMonitorTraceListerner.cs
@@ -0,0 +1,45 @@
+﻿/*//////////////////////////////////////////////////////////////////////////////
+// ProcessMonitorTraceListener
+// 
+// History:
+// - April 1, 2010 - Version 1.0 - John Robbins/Wintellect
+//      - Initial release.
+// - March 1, 2014 - Version 1.1 - John Robbins/Wintellect
+//      - Moved to VS 2013 and .NET 4.5.1
+//////////////////////////////////////////////////////////////////////////////*/
+
+namespace Sysinternals.Debug
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    /// <summary>
+    /// Implements a <see cref="TraceListener"/> that redirects output to 
+    /// Sysinternal's Process Monitor program.
+    /// </summary>
+    public class ProcessMonitorTraceListener : TraceListener
+    {
+        /// <summary>
+        /// Writes a trace message to Process Monitor.
+        /// </summary>
+        /// <param name="message">
+        /// A message to write.
+        /// </param>
+        public override void Write(string message)
+        {
+            NativeMethods.ProcMonDebugOutput(message);
+        }
+
+        /// <summary>
+        /// Writes a trace message to Process Monitor.
+        /// </summary>
+        /// <param name="message">
+        /// A message to write.
+        /// </param>
+        public override void WriteLine(string message)
+        {
+            NativeMethods.ProcMonDebugOutput(message);
+        }
+    }
+}
---
+++ Sysinternals.Debug/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: CLSCompliant(true)]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sysinternals.Debug")]
+[assembly: AssemblyDescription("Send Trace output to Process Monitor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wintellect")]
+[assembly: AssemblyProduct("Sysinternals.Debug")]
+[assembly: AssemblyCopyright("Copyright 2014 John Robbins/Wintellect")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: SecurityCritical]
+
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c8467a9a-9e22-41cf-aaf1-114c9ee949b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
